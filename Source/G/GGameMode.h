d0c79ba28c4544cd9f973a485f06b1c5508af3b8
Kevin Chen <k.chen8880@gmail.com>
2016-09-24 01:40:05 -0400

    Initial commit, UE4 setup

---
+++ Source/G/GGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "GGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class G_API AGGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
